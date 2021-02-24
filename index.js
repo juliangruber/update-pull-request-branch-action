321d455e7fa8de7b5d7b2ba8ae63049e9b80d2b0
Robert Egglestone <robertegglestone@gmail.com>
2021-02-25 03:03:44 +1300

    update all that need it

--- index.js
+++ index.js
@@ -3,25 +3,16 @@
 const core = require('@actions/core')
 const { GitHub, context } = require('@actions/github')
 
-const sleep = dt => new Promise(resolve => setTimeout(resolve, dt))
-
 const main = async () => {
-  const token = core.getInput('github-token')
-  const numberInput = core.getInput('number')
-  const wait = core.getInput('wait') === 'true'
+  const token = core.getInput('github_token')
 
   const octokit = new GitHub(token)
 
-  let numbers
-  if (numberInput != null) {
-    numbers = [numberInput]
-  } else {
-    const listRes = await octokit.pulls.list({
-      ...context.repo,
-      state: 'open'
-    })
-    numbers = listRes.data.map(pull => pull.number)
-  }
+  const listRes = await octokit.pulls.list({
+    ...context.repo,
+    state: 'open'
+  })
+  const numbers = listRes.data.map(pull => pull.number)
 
   for (const number of numbers) {
     const res = await octokit.pulls.get({
@@ -29,27 +20,15 @@ const main = async () => {
       pull_number: number
     })
     const oldSha = res.data.head.sha
-    const rebaseable = res.data.rebaseable
 
-    if (rebaseable) {
+    if (res.data.mergeable && res.data.mergeable_state === 'behind') {
+      console.log(`Updating pull request ${number}`)
+
       await octokit.pulls.updateBranch({
         ...context.repo,
         pull_number: number,
         expected_head_sha: oldSha
       })
-
-      if (wait) {
-        while (true) {
-          const res = await octokit.pulls.get({
-            ...context.repo,
-            pull_number: number
-          })
-          if (res.data.head.sha !== oldSha) return
-
-          core.debug('sleep')
-          await sleep(1000)
-        }
-      }
     }
   }
 }
