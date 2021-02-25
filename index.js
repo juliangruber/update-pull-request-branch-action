631b853ae40e1e61d7d09d57b954db03009dfb1c
Robert Egglestone <robertegglestone@gmail.com>
2021-02-26 01:39:13 +1300

    Code style fix

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const main = async () => {
   // GitHub token, also check underscore notation for console testing
   const token = core.getInput('github-token') || core.getInput('github_token')
   const number = core.getInput('number')
-  const waitForPullRequestUpdated = core.getInput('waitForPullRequestUpdated') === 'true'
+  const waitForPullRequestUpdated =
+    core.getInput('waitForPullRequestUpdated') === 'true'
 
   const octokit = new GitHub(token)
 
