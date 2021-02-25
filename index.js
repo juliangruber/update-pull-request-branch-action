f32b9c9e22af36a7570d032eccaa967055f8a760
Robert Egglestone <robertegglestone@gmail.com>
2021-02-25 23:15:17 +1300

    Style conformance

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const main = async () => {
             ...context.repo,
             pull_number: number
           })
-          prUpdated = (res.data.head.sha === oldSha)
+          prUpdated = res.data.head.sha === oldSha
         }
       }
     } else {
