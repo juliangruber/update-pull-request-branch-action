89b107223df329b81c724ea8dbc79ac195f66618
Robert Egglestone <robertegglestone@gmail.com>
2021-02-26 03:36:44 +1300

    Feature to update all pull requests asynchronously (#9)
    * Update action.yml
    
    * Update index.js
    
    * Update action.yml
    
    * Apply changes
    
    * 1.1.0
    
    * update all that need it
    
    * build dist
    
    * 1.1.1
    
    * Update README.md
    
    * Realign to upstream code for pull request
    
    * Revert package-lock.json
    
    * Revert dist/index.js
    
    * Style conformance
    
    * Update README.md
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>
    
    * Change async to waitForPullRequestUpdated
    
    * Revert readme wording
    
    * Code style fix
    
    * Fix condition on wait loop
    
    * Use github_token
    
    * Fix code comment to be accurate
    
    * Use break rather than loop state
    
    * Update README.md
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- README.md
+++ README.md
@@ -2,18 +2,29 @@
 
 Update a Pull Request from its base branch.
 
-After updating it waits until the branch has been successfully updated,
+Optionally waits until the branch has been successfully updated,
 as the REST api route [doesn't](https://developer.github.com/v3/pulls/#response-3).
 
 ## Usage
 
+To update a single pull request and wait until it's complete:
 ```yaml
 steps:
   - name: Update Pull Request
     uses: juliangruber/update-pull-request-branch-action@v1
     with:
       number: 1
-      github-token: ${{ secrets.GITHUB_TOKEN }}
+      github_token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+To update all open pull requests without waiting:
+```yaml
+steps:
+  - name: Update All Pull Requests
+    uses: juliangruber/update-pull-request-branch-action@v1
+    with:
+      waitForPullRequestUpdated: false
+      github_token: ${{ secrets.GITHUB_TOKEN }}
 ```
 
 ## License
--- index.js
+++ index.js
@@ -6,32 +6,59 @@ const { GitHub, context } = require('@actions/github')
 const sleep = dt => new Promise(resolve => setTimeout(resolve, dt))
 
 const main = async () => {
-  const token = core.getInput('github-token')
+  const token = core.getInput('github_token')
   const number = core.getInput('number')
+  const waitForPullRequestUpdated =
+    core.getInput('waitForPullRequestUpdated') === 'true'
 
   const octokit = new GitHub(token)
 
-  const res = await octokit.pulls.get({
-    ...context.repo,
-    pull_number: number
-  })
-  const oldSha = res.data.head.sha
+  let numbers
+  if (number) {
+    numbers = [number]
+  } else {
+    const listRes = await octokit.pulls.list({
+      ...context.repo,
+      state: 'open'
+    })
+    numbers = listRes.data.map(pull => pull.number)
+  }
 
-  await octokit.pulls.updateBranch({
-    ...context.repo,
-    pull_number: number,
-    expected_head_sha: oldSha
-  })
-
-  while (true) {
+  for (const number of numbers) {
     const res = await octokit.pulls.get({
       ...context.repo,
       pull_number: number
     })
-    if (res.data.head.sha !== oldSha) return
+    const oldSha = res.data.head.sha
+    const prMergeable = res.data.mergeable
+    const prMergeableState = res.data.mergeable_state
 
-    core.debug('sleep')
-    await sleep(1000)
+    // update pull request only if it's outdated and there are no conflicts
+    if (prMergeable && prMergeableState === 'behind') {
+      core.info(`Updating pull request ${number}`)
+
+      await octokit.pulls.updateBranch({
+        ...context.repo,
+        pull_number: number,
+        expected_head_sha: oldSha
+      })
+
+      if (waitForPullRequestUpdated) {
+        while (true) {
+          core.debug('sleep')
+          await sleep(1000)
+
+          const res = await octokit.pulls.get({
+            ...context.repo,
+            pull_number: number
+          })
+
+          if (res.data.head.sha !== oldSha) break
+        }
+      }
+    } else {
+      core.info(`Skipping ${prMergeableState} pull request ${number}`)
+    }
   }
 }
 
